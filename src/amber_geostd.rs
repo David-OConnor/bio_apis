c582d8cf368f5ad4f4fa9aa3b0f0fbbb9abb74f9
Data <the_alchemist@fastmail.com>
2025-10-03 14:46:15 -0400

    sync

--- src/amber_geostd.rs
+++ src/amber_geostd.rs
@@ -26,7 +26,7 @@ pub struct GeostdData {
     pub mol2: String,
     pub frcmod: Option<String>,
     pub lib: Option<String>,
-    pub ident_pubchem: Option<u32>
+    pub pubchem_cid: Option<u32>
 }
 
 #[derive(Clone, Debug, Deserialize)]
